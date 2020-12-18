ec0ef26f102ffc455fb610c8967e2eb30ef6e9d6
zenocoz <federico.soncini@gmail.com>
2020-12-18 11:47:54 +0100

    error validators for post

--- src/services/media/index.js
+++ src/services/media/index.js
@@ -11,10 +11,59 @@ const router = express.Router()
 //Paths
 const mediaFilesPath = join(__dirname, "media.json")
 
-router.get("/", async (req, res, next) => {})
-router.post("/", async (req, res, next) => {})
+router.get(
+  "/",
+
+  async (req, res, next) => {
+    try {
+      console.log("reached")
+    } catch (error) {
+      next(error)
+    }
+  }
+)
+router.post(
+  "/",
+  [
+    check("Title")
+      .exists()
+      .withMessage("You need a title for your movie")
+      .isLength({ min: 1 })
+      .withMessage("Could be a one char movie like P"),
+    check("Year").exists().withMessage("year of production not found"),
+    check("imdbID").exists().withMessage("imdbID not found"),
+    check("Type").exists().withMessage("Media type not found"),
+  ],
+  async (req, res, next) => {
+    try {
+      const errors = validationResult(req)
+      if (!errors.isEmpty()) {
+        const err = new Error()
+        err.message = errors
+        err.httpStatusCode = 400
+        next(err)
+      } else {
+        const mediaArray = await readDB(mediaFilesPath)
+        const newMedia = {
+          ...req.body,
+          createdAt: new Date(),
+        }
+        mediaArray.push(newMedia)
+        await writeDB(mediaFilesPath, mediaArray)
+        res
+          .status(201)
+          .send({ "created new entry for imdbID": newMedia.imdbID })
+      }
+    } catch (error) {
+      console.log(error)
+      next(error)
+    }
+  }
+)
 router.put("/", async (req, res, next) => {})
 router.delete("/", async (req, res, next) => {})
+
+//reviews
 
 //media image
 router.post("/:id/upload", async (req, res, next) => {})
